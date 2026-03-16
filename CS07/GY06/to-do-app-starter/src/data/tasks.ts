7db41e7eb267b4c78fb7f04c5aaa40822c13d71b
baguadam <m3wdo5@inf.elte.hu>
2026-03-16 08:14:33 +0100

    Update to-do-app tasks

--- CS07/GY06/to-do-app-starter/src/data/tasks.ts
+++ CS07/GY06/to-do-app-starter/src/data/tasks.ts
@@ -8,7 +8,7 @@ const tasks: Task[] = [
   { id: 1, title: "Bemenni kétszer az irodába", completed: false },
   { id: 2, title: "Egyenesbe jönni a tárgyaimmal", completed: false },
   { id: 3, title: "Elmenni csütörtöki focira", completed: true },
-  { id: 4, title: "Logolni valamit Stravara", completed: true },
+  { id: 4, title: "Letörölni a Facebookot", completed: true },
 ];
 
 export default tasks;
